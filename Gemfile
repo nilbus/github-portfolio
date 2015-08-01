e97f0e0a95487f4422bb32a83c8a14cc591c3c6d
Edward Anderson <nilbus@nilbus.com>
2015-07-31 22:39:07 -0400

    Bundle; Remove some testing gems I won't be using

--- Gemfile
+++ Gemfile
@@ -43,13 +43,10 @@ group :development, :test do
 end
 
 group :test do
-  gem "capybara-webkit", ">= 1.2.0"
   gem "database_cleaner"
-  gem "formulaic"
   gem "launchy"
   gem "shoulda-matchers", require: false
   gem "simplecov", require: false
-  gem "timecop"
   gem "webmock"
 end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,17 +7,10 @@ require "shoulda/matchers"
 
 Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
 
-module Features
-  # Extend this module in spec/support/features/*.rb
-  include Formulaic::Dsl
-end
-
 RSpec.configure do |config|
-  config.include Features, type: :feature
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = false
 end
 
 ActiveRecord::Migration.maintain_test_schema!
-Capybara.javascript_driver = :webkit
