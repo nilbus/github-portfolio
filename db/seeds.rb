6755fa89c212cc67cec15b3598daa8b4887a538a
Edward Anderson <nilbus@nilbus.com>
2015-08-04 21:13:38 -0400

    Begin building fixture data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.new(
+  name: 'Linda Hand',
+  login: 'test'
+)
+portfolio = Portfolio.new(
+  header: Header.generic(user),
+  user: user
+)
+PortfolioStore.new.save(portfolio)
