f2563d93da44d944a4811c82eab729bd5f5db4a7
Edward Anderson <nilbus@nilbus.com>
2015-08-04 20:28:33 -0400

    Fix portfolios loading screen spec via case insensitivity

--- spec/controllers/portfolios_controller_spec.rb
+++ spec/controllers/portfolios_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PortfoliosController, type: :controller do
   it "shows a loading screen when there is no data for the requested user" do
     github_username = 'unseen_user'
     get :show, id: github_username
-    expect(response.body).to match /wait|load|fetch|get/
+    expect(response.body).to match /wait|load|fetch|get/i
     expect(response.body).to include github_username
   end
 
