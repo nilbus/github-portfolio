f870d253186e5b7d52bbabb69e7389f98ce9fde0
Edward Anderson <nilbus@nilbus.com>
2015-08-07 10:17:15 -0400

    Extract class GithubAPI::ResponseObjectConverter

--- app/models/github_api.rb
+++ app/models/github_api.rb
@@ -4,9 +4,11 @@
 # aggregate calls to fetch a single piece of information, when the API does not
 # provide that information directly.
 #
-class GithubAPI # rubocop:disable Metrics/ClassLength
+class GithubAPI
   def initialize(github_username:, token: nil, cache: true)
     @github_username = github_username
+    querying_user = User.new(login: github_username)
+    @object_from = ResponseObjectConverter.new(querying_user: querying_user)
     @api = Octokit::Client.new access_token: token
     @api.auto_paginate = true
     @api.per_page = 100
@@ -20,14 +22,14 @@ class GithubAPI # rubocop:disable Metrics/ClassLength
 
   # Filtering for private repos is unnecessary w/ only public_repo access
   def public_repos
-    @api.repositories(@github_username, all: true).map do |repo|
-      repo_from_response(repo)
+    @api.repositories(@github_username, all: true).map do |response_repo|
+      @object_from.repo(response_repo)
     end
   end
 
   def starred_repos
-    @api.starred(@github_username).map do |repo|
-      repo_from_response(repo)
+    @api.starred(@github_username).map do |response_repo|
+      @object_from.repo(response_repo)
     end
   end
 
@@ -46,27 +48,44 @@ class GithubAPI # rubocop:disable Metrics/ClassLength
 
   def substitute_parent_for_fork(repo)
     if repo.fork
-      repo_from_response(@api.repo(repo.full_name).parent)
+      @object_from.repo(@api.repo(repo.full_name).parent)
     else
       repo
     end
   end
 
   def user_issues(repo:)
-    @api.list_issues(repo.full_name, state: :all).map do |issue_response|
-      issue_from_response(issue_response, repo_full_name: repo.full_name)
+    @api.list_issues(repo.full_name, state: :all).map do |response_issue|
+      @object_from.issue(response_issue, repo_full_name: repo.full_name)
     end
   end
 
   # Given an Issue, return a new Issue with closed_by detail
-  def issue_detail(issue:)
+  def issue_detail(issue:) # rubocop:disable Metrics/MethodLength
     issue_response = @api.issue(issue.repo, issue.number)
-    issue_from_response(issue_response, repo_full_name: issue.repo)
+    if issue_response.state == 'open'
+      user_comment_count = issue_comment_count_by_user(
+        issue_response: issue_response,
+        repo_full_name: issue.repo,
+      )
+    end
+    @object_from.issue(
+      issue_response,
+      repo_full_name: issue.repo,
+      user_comment_count: user_comment_count,
+    )
   end
 
   def user_commits(repo:)
     commits_response = @api.commits(repo.full_name, author: @github_username)
-    commits_response.map { |response_commit| commit_from_response(response_commit) }
+    commits_response.map { |response_commit| @object_from.commit(response_commit) }
+  end
+
+  def issue_comment_count_by_user(issue_response:, repo_full_name:)
+    comments = with_max(100) do
+      @api.issue_comments(repo_full_name, issue_response.number)
+    end
+    comments.count { |comment| comment.user.login == @github_username }
   end
 
   def with_max(max)
@@ -82,67 +101,6 @@ class GithubAPI # rubocop:disable Metrics/ClassLength
   end
 
   private
-
-  # rubocop:disable Metrics/MethodLength
-  def repo_from_response(response)
-    Repo.new(
-      full_name: response.full_name,
-      name: response.name,
-      description: response.description,
-      created_at: response.created_at,
-      fork: response.fork,
-      owner: user_from_response_user(response.owner),
-      primary_language: response.language,
-      querying_user: User.new(login: @github_username),
-      star_count: response.stargazers_count,
-      url: response.html_url,
-    )
-  end
-
-  # rubocop:disable Metrics/AbcSize
-  def issue_from_response(response, repo_full_name:)
-    if response.state == 'open'
-      user_comment_count = issue_comment_count_by_user(
-        issue_response: response,
-        repo_full_name: repo_full_name,
-      )
-    end
-    Issue.new(
-      number: response.number,
-      title: response.title,
-      assigned_to: user_from_response_user(response.assignee),
-      closed_by: user_from_response_user(response.closed_by),
-      created_by: user_from_response_user(response.user),
-      has_user_commentary: user_comment_count && user_comment_count > 0,
-      pull_request: response.pull_request.present?,
-      repo: repo_full_name,
-      state: response.state,
-      url: response.html_url,
-    )
-  end
-  # rubocop:enable Metrics/AbcSize
-  # rubocop:enable Metrics/MethodLength
-
-  def user_from_response_user(response_user)
-    return nil unless response_user.present?
-    User.new(login: response_user.login, name: response_user.name.presence)
-  end
-
-  def issue_comment_count_by_user(issue_response:, repo_full_name:)
-    comments = with_max(100) do
-      @api.issue_comments(repo_full_name, issue_response.number)
-    end
-    comments.count { |comment| comment.user.login == @github_username }
-  end
-
-  def commit_from_response(response_commit)
-    Commit.new(
-      sha: response_commit.sha,
-      author: user_from_response_user(response_commit.author),
-      message: response_commit.commit.message,
-      url: response_commit.html_url,
-    )
-  end
 
   def configure_cache
     @api.middleware = prepend_middleware_to(Octokit.middleware) do |builder|
---
+++ app/models/github_api/response_object_converter.rb
@@ -0,0 +1,55 @@
+# Convert Octokit API responses into ValueObject/Entity objects
+#
+# rubocop:disable Style/ClassAndModuleChildren
+#
+class GithubAPI::ResponseObjectConverter
+  def initialize(querying_user:)
+    @querying_user = querying_user
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  def repo(response)
+    Repo.new(
+      full_name: response.full_name,
+      name: response.name,
+      description: response.description,
+      created_at: response.created_at,
+      fork: response.fork,
+      owner: user(response.owner),
+      primary_language: response.language,
+      querying_user: @querying_user,
+      star_count: response.stargazers_count,
+      url: response.html_url,
+    )
+  end
+
+  def issue(response, repo_full_name:, user_comment_count: nil)
+    Issue.new(
+      number: response.number,
+      title: response.title,
+      assigned_to: user(response.assignee),
+      closed_by: user(response.closed_by),
+      created_by: user(response.user),
+      has_user_commentary: user_comment_count && user_comment_count > 0,
+      pull_request: response.pull_request.present?,
+      repo: repo_full_name,
+      state: response.state,
+      url: response.html_url,
+    )
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  def user(response_user)
+    return nil unless response_user.present?
+    User.new(login: response_user.login, name: response_user.name.presence)
+  end
+
+  def commit(response_commit)
+    Commit.new(
+      sha: response_commit.sha,
+      author: user(response_commit.author),
+      message: response_commit.commit.message,
+      url: response_commit.html_url,
+    )
+  end
+end
