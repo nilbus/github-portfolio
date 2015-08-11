5219920af1ff5db4d6a1cd374e0c8b6e82cb0a30
Edward Anderson <nilbus@nilbus.com>
2015-08-10 20:49:41 -0400

    Refactor UserRole#role_matchers for readability

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -7,21 +7,53 @@ class UserRole
   end
 
   def to_s
-    case
-    when stats.commit_count_user == stats.commit_count_total
-      'Sole Author'
-    when stats.commits_authored_percentage >= 75
-      'Main Author'
-    when stats.commits_authored_percentage >= 50
-      'Primary Contributor'
-    when stats.contribution_rank_by_commits == 1
-      'Largest Contributor'
-    when stats.contribution_rank_by_commits <= stats.total_contributors / 2.0
-      "Top #{stats.tier} Contributor"
-    when stats.commits_authored_percentage >= 20 || stats.commit_count_user >= 10
-      'Solid Contributor'
-    else
-      'Casual Contributor'
-    end
+    role_matchers.find { |condition, _role| condition.call }[1]
+  end
+
+  private
+
+  # Matchers ordered by precedence
+  def role_matchers
+    {
+      -> { only_one_committer }         => 'Sole Author',
+      -> { authored_75_percent }        => 'Main Author',
+      -> { authored_50_percent }        => 'Primary Contributor',
+      -> { highest_commit_count }       => 'Largest Contributor',
+      -> { top_half_committers }        => "Top #{@stats.tier} Contributor",
+      -> { decent_commit_contribution } => 'Solid Contributor',
+      -> { true }                       => 'Casual Contributor',
+    }
+  end
+
+  def only_one_committer
+    @stats.commit_count_user == @stats.commit_count_total
+  end
+
+  def authored_75_percent
+    @stats.commits_authored_percentage >= 75
+  end
+
+  def authored_50_percent
+    @stats.commits_authored_percentage >= 50
+  end
+
+  def highest_commit_count
+    @stats.contribution_rank_by_commits == 1
+  end
+
+  def top_half_committers
+    @stats.contribution_rank_by_commits <= @stats.total_contributors / 2.0
+  end
+
+  def decent_commit_contribution
+    decent_commit_percentage || decent_commit_count
+  end
+
+  def decent_commit_percentage
+    @stats.commits_authored_percentage >= 20
+  end
+
+  def decent_commit_count
+    @stats.commit_count_user >= 10
   end
 end
