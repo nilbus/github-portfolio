dd081b603785bade12c9700e988d79221b778001
Edward Anderson <nilbus@nilbus.com>
2018-10-31 22:58:40 -0400

    Resolve Metrics/LineLength violations

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,7 +2,18 @@
 
 # A GitHub Issue / Pull Request value object
 class Issue
-  include ValueObject.new(:number, :title, :assigned_to, :closed_by, :created_by, :has_user_commentary, :pull_request, :repo, :state, :url)
+  include ValueObject.new(
+    :number,
+    :title,
+    :assigned_to,
+    :closed_by,
+    :created_by,
+    :has_user_commentary,
+    :pull_request,
+    :repo,
+    :state,
+    :url,
+  )
 
   def resolved_by?(user)
     return false unless resolved?
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -4,7 +4,29 @@
 #
 class Repo
   include Entity.new(:full_name)
-  attr_accessor(:created_at, :description, :fork, :full_name, :issues, :issues_url, :primary_language, :languages, :name, :owner, :releases_url, :reporting_period, :star_count, :url, :querying_user, :stats, :user_is_collaborator, :user_comments, :user_commits, :user_commits_url, :version)
+  attr_accessor(
+    :created_at,
+    :description,
+    :fork,
+    :full_name,
+    :issues,
+    :issues_url,
+    :languages,
+    :name,
+    :owner,
+    :primary_language,
+    :querying_user,
+    :releases_url,
+    :reporting_period,
+    :star_count,
+    :stats,
+    :url,
+    :user_comments,
+    :user_commits,
+    :user_commits_url,
+    :user_is_collaborator,
+    :version,
+  )
 
   def description
     @description.try(&:strip) || ''
